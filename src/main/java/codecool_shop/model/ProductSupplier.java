e3d2bc75ee4776987d266da748d6e6881f058034
GitGitarra <monika.plocica@gmail.com>
2017-05-16 23:15:08 +0200

    Add ProductSupplier model with constructor and getter methods

---
+++ src/main/java/codecool_shop/model/ProductSupplier.java
@@ -0,0 +1,32 @@
+package codecool_shop.model;
+
+/**
+ * Created by monika on 16.05.17.
+ */
+public class ProductSupplier {
+    private Integer id;
+    private String name;
+    private String address;
+
+    public ProductSupplier() {
+
+    }
+
+    public ProductSupplier(Integer id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
