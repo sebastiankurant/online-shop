908691f1f17716da2523681b44c9677bce7e25da
Piotr <pgurdek@gmail.com>
2017-05-16 11:59:34 +0200

    Merged CRUD and Shop project

--- README.md
+++
@@ -1,8 +0,0 @@
-# Codecool Online Shop
-
-Java SE: 2nd TW week, Project skeleton
-
-# Install
-
-Import this project to IntelliJ as a Maven project.
-IntelliJ can auto-install the dependencies from the pom.xml
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,112 @@
-import com.codecool.shop.controller.MainMenuController;
-import com.codecool.shop.controller.ProductController;
-import com.codecool.shop.dao.SqliteJDBCConnector;
+import controller.*;
+import dao.SgliteJDSCConnector;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
+
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
 
 public class Main {
 
+
     public static void main(String[] args) {
-        if(args.length > 0 && args[0].equals("--create-tables")) {
-            try {
-                SqliteJDBCConnector.createTables();
-            } catch (SQLException e) {
-                System.out.println("Cannot create tables in DB");
-                System.out.println();
-            }
+        try {
+            SgliteJDSCConnector temp = new SgliteJDSCConnector();
+            temp.createTables();
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        exception(Exception.class, (e, req, res) -> e.printStackTrace());
+
+        Boolean localhost = true;  //Prevent public files from caching
+        if (localhost) {
+            String projectDir = System.getProperty("user.dir");
+            String staticDir = "/src/main/resources/public";
+            staticFiles.externalLocation(projectDir + staticDir);
+        } else {
+            staticFileLocation("/public");
         }
 
+        port(8888);
+        enableDebugScreen();
+        ProductController productController = new ProductController();
+        ProductControllerAdmin eventControllerAdmin = new ProductControllerAdmin();
+        CategoryControllerAdmin catController = new CategoryControllerAdmin();
+        LoginController loginController = new LoginController();
+        AdminController adminController = new AdminController();
 
-        MainMenuController mainMenuController = new MainMenuController();
-        mainMenuController.mainMenuAction();
+
+        path("/", () -> {
+            before("/*", (req, res) -> { // Ensure that url have "/" on ened
+                String path = req.pathInfo();
+                if (!path.endsWith("/")) {
+                    res.redirect(path + "/");
+                }
+            });
+            get("/", productController::displayEvents, new ThymeleafTemplateEngine());
+
+//            Front End routes - Not secured routes
+
+            path("/login/", () -> {
+                before((request, response) -> {
+                });
+                get("/", loginController::serveLoginPage, new ThymeleafTemplateEngine());
+                post("/", loginController::handleLoginPost, new ThymeleafTemplateEngine());
+            });
+
+//            Admin routes
+
+            path("/admin/", () -> {
+                before("/*", (req, res) -> {
+                    loginController.ensureUserIsLoggedIn(req, res);
+                });
+
+                get("/", adminController::displayIndex, new ThymeleafTemplateEngine());
+                post("/logout/", loginController::handleLogoutPost);
+                path("/products", () -> {
+                    get("/", eventControllerAdmin::renderEvents, new ThymeleafTemplateEngine());
+                    get("/add/", eventControllerAdmin::addEvent, new ThymeleafTemplateEngine());
+                    get("/edit/:id/", eventControllerAdmin::editEvent, new ThymeleafTemplateEngine());
+                    get("/past/", eventControllerAdmin::pastEvents, new ThymeleafTemplateEngine());
+                    get("/category/", eventControllerAdmin::filterCategory, new ThymeleafTemplateEngine());
+                    post("/add/", eventControllerAdmin::addEventPost, new ThymeleafTemplateEngine());
+                    post("/remove/:id/", eventControllerAdmin::removeEvent);
+                    post("/edit/:id/", eventControllerAdmin::editEventPost);
+                });
+
+                path("/category", () -> {
+                    get("/", catController::renderCategory, new ThymeleafTemplateEngine());
+                    get("/add/", catController::addCategory, new ThymeleafTemplateEngine());
+                    get("/edit/:id/", catController::editCategory, new ThymeleafTemplateEngine());
+                    post("/add/", catController::addCategoryPost, new ThymeleafTemplateEngine());
+                    post("/edit/:id/", catController::editCategoryPost, new ThymeleafTemplateEngine());
+                    post("/remove/:id/", catController::removeCategory);
+
+                });
+            });
+        });
+
+
+        // Using string/html to display not correct routes
+        notFound("<!doctype html>\n" +
+                "<html xmlns:th=\"http://www.thymeleaf.org\">\n" +
+                "\n" +
+                "<head th:replace=\"header :: copy\">\n" +
+                "    <title>404</title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>\n" +
+                "<div class=\"container\">\n" +
+                "    <h1>404</h1>\n"+
+                "    <h3>Not Found</h3>\n" +
+                "</div>\n" +
+                "<div th:replace=\"footer :: copy\"></div>\n" +
+                "</body>\n" +
+                "</html>");
+
     }
-
 
 }
