6ef12c4f06be234e8ae10a1232b2f980f35fe5e5
sebastiankurant <sebastian.kt7@gmail.com>
2017-05-17 14:34:05 +0200

    DataBase
    Create restartTables and fillIfNotExistTables methods to handle manipulate database

--- src/main/java/codecool_shop/Application.java
+++ src/main/java/codecool_shop/Application.java
@@ -28,14 +28,30 @@ public class Application {
         return app;
     }
 
-    public void start() {
+    public static void restartTables(){
         try {
             SgliteJDSCConnector temp = new SgliteJDSCConnector();
+            temp.dropTables();
             temp.createTables();
         } catch (SQLException e) {
-
             e.printStackTrace();
+            System.out.println("Can't drop and add tables.");
+            System.exit(1);
         }
+    }
+
+    public static void fillIfNotExistTables(){
+        try {
+            SgliteJDSCConnector temp = new SgliteJDSCConnector();
+            temp.createTablesIfNotExist();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Can't fill with not exist tables.");
+            System.exit(1);
+        }
+    }
+
+    public void start() {
 
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         Boolean localhost = true;  //Prevent public files from caching
