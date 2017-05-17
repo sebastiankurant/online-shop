c77e3e0f50c8ced3fac22d564cd2493c258b8a78
GitGitarra <monika.plocica@gmail.com>
2017-05-17 11:22:08 +0200

    Add to supplier admin controller renderSupplier add and edit methods

--- src/main/java/codecool_shop/controller/SupplierControllerAdmin.java
+++ src/main/java/codecool_shop/controller/SupplierControllerAdmin.java
@@ -2,6 +2,7 @@ package codecool_shop.controller;
 
 import codecool_shop.dao.SupplierDao;
 import codecool_shop.dao.SupplierInterface;
+import codecool_shop.model.ProductSupplier;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -20,11 +21,27 @@ public class SupplierControllerAdmin {
     public ModelAndView renderSupplier(Request req, Response res) {
         Map<Object, Object> params = new HashMap<>();
         try {
-            params.put("categoryContainer", supplierDao.getAll());
+            params.put("supplierContainer", supplierDao.getAll());
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return new ModelAndView(params, "admin/supplier/index");
+    }
+
+    public ModelAndView add(Request req, Response res) {
+        Map<Object, Object> params = new HashMap<>();
+        return new ModelAndView(params, "admin/supplier/add");
+    }
+
+    public ModelAndView editSupplier(Request req, Response res) throws SQLException  {
+        Integer id = Integer.valueOf(req.params("id"));
+        ProductSupplier productSupplier = supplierDao.getById(id);
+        Map params = new HashMap<>();
+        if (!(productSupplier == null)) {
+            params.put("supplierContainer", productSupplier);
+            return new ModelAndView(params, "admin/supplier/edit");
+        }
+        return new ModelAndView(params, "404");
     }
 
 }
