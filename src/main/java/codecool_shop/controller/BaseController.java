5b4f640b9d20160e217234d6ce1eb88043e9c035
sebastiankurant <sebastian.kt7@gmail.com>
2017-05-17 15:36:52 +0200

    Methods
    Add render method

--- src/main/java/codecool_shop/controller/BaseController.java
+++ src/main/java/codecool_shop/controller/BaseController.java
@@ -1,7 +1,21 @@
 package codecool_shop.controller;
 
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Galileo on 5/17/17.
  */
 public class BaseController {
+
+	protected ModelAndView render(Map<String, Object> parameters, String viewName){
+		return new ModelAndView(parameters, viewName);
+	}
+
+	protected ModelAndView render(String viewName){
+		Map<String, Object> parameters = new HashMap<>();
+		return new ModelAndView(parameters, viewName);
+	}
 }
