ebb366c470997d7bb83c988a4704b76e3d9f0cfe
sebastiankurant <sebastian.kt7@gmail.com>
2017-05-17 14:32:40 +0200

    DataBase
    Change main to handle parameters from command line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,17 @@
 import codecool_shop.Application;
-import codecool_shop.controller.*;
-import codecool_shop.dao.SgliteJDSCConnector;
-import spark.template.thymeleaf.ThymeleafTemplateEngine;
-
-import java.sql.SQLException;
-
-import static spark.Spark.*;
-import static spark.debug.DebugScreen.enableDebugScreen;
 
 
 public class Main {
 
 
-    public static void main(String[] args) {
-        Application.runApp().start();
-    }
+	public static void main(String[] args) {
+		if (args.length > 0 && args[0].equals("--init-db")) {
+			Application.restartTables();
+
+		} else if (args.length > 0 && args[0].equals("--migrate-db")) {
+			Application.fillIfNotExistTables();
+		}
+		Application.runApp().start();
+	}
 
 }
