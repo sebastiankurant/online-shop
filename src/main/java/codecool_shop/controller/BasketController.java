504041cfc430941bb0a3d47bd3cead42ec35df96
Piotr <pgurdek@gmail.com>
2017-05-17 15:34:03 +0200

    Add removeProduct method , improve getBasket method

--- src/main/java/codecool_shop/controller/BasketController.java
+++ src/main/java/codecool_shop/controller/BasketController.java
@@ -3,16 +3,14 @@ package codecool_shop.controller;
 import codecool_shop.dao.ProductDao;
 import codecool_shop.dao.ProductInterface;
 import codecool_shop.model.Product;
-import spark.*;
-import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 
-import javax.servlet.http.Cookie;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static spark.Spark.get;
 
 /**
  * Created by pgurdek on 16.05.17.
@@ -22,30 +20,45 @@ public class BasketController {
     private ProductInterface produtDao = new ProductDao();
     private ProductController productController = new ProductController();
 
-    public ModelAndView getBasket(Request req, Response res){
-        Map<String,Object> params = new HashMap<>();
-        return new ModelAndView(params,"basket/basket");
+    public ModelAndView getBasket(Request req, Response res) {
+        Map<String, Object> params = new HashMap<>();
+        List<Product> basketProductList;
+        basketProductList = req.session().attribute("basketProductList");
+        if (!(basketProductList == null)) {
+            params.put("basket", basketProductList);
+            params.put("removeFromBasket", req.session().attribute("removeFromBasket"));
+            req.session().attribute("removeFromBasket",false);
+        }
+
+        return new ModelAndView(params, "basket/basket");
     }
 
     public String addToBasket(Request req, Response res) throws SQLException {
         Integer id = Integer.valueOf(req.queryParams("add_product"));
-        System.out.println(id);
         List<Product> basketProductList;
         basketProductList = req.session().attribute("basketProductList");
-        basketProductList.add(produtDao.getById(id));
-        req.session().attribute("addedToCart",true);
-        for (Product pro:basketProductList){
-            System.out.println(pro.getName());
-        }
+        Product tmpProduct = produtDao.getById(id);
+        basketProductList.add(tmpProduct);
+        req.session().attribute("basketProductList", basketProductList);
+        req.session().attribute("addedToCart", true);
+        req.session().attribute("productName", tmpProduct.getName());
         res.redirect("/");
         return "";
+    }
 
-//        RouteImpl route   = new RouteImpl();
-//        return route.handle(req, res);
-//
-//        return (request, responses) -> {
-//            responses.redirect("/");
-//            return null;
-//        };
+    public String removeProduct(Request req, Response res) throws SQLException {
+        Integer id = Integer.valueOf(req.queryParams("remove_product"));
+        List<Product> basketProductList;
+        basketProductList = req.session().attribute("basketProductList");
+        for (Product product : basketProductList) {
+            if (product.getId() == id) {
+                basketProductList.remove(product);
+                req.session().attribute("removeFromBasket", true);
+                break;
+            }
+        }
+        req.session().attribute("basketProductList", basketProductList);
+        res.redirect("/basket/");
+        return "";
     }
 }
